2fe16477f7d0e080aee1633bb24d4e5a524d645c
= <=>
2025-01-29 10:22:22 +0000

    before lifting button up

---
+++ src/components/Item_2_of_NavBarSelection/new_page_brent_and_dated_with_button.tsx
@@ -0,0 +1,7 @@
+import {Table_brent_dated} from "./Table_brent_spreads_with_button_new.tsx"
+
+
+
+export function Two_tables_dated_and_etf_and_button () {
+    return <Table_brent_dated />
+}
--- src/components/Item_2_of_NavBarSelection/page_brent.tsx
+++ src/components/Item_2_of_NavBarSelection/page_brent.tsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import {Table_brent_spreads_with_button} from "./page_brent_new.tsx"
 import Table_spreads from "./page_brent_child_table_1.tsx"
 import Tables_dated_and_etf from "./page_brent_child_table_2.tsx"
-
+import {Table_brent_dated} from "./Table_brent_spreads_with_button_new.tsx"
+import {Two_tables_dated_and_etf_and_button} from "./new_page_brent_and_dated_with_button.tsx"
 
 const MyTables = () => {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
+
       <div>
         <Table_brent_spreads_with_button />
       </div>
@@ -17,6 +19,9 @@ const MyTables = () => {
       </div> */}
 
       <div style={{ flex: 1, borderLeft: '1px solid #ddd' }}> {/* border between left and right side*/}
+        <div>
+          <Two_tables_dated_and_etf_and_button />
+        </div>
         <div >
           <Tables_dated_and_etf />
         </div>
